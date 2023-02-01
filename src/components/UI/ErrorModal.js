94b04b53c0395567d8ea92bb5c06d34e8d3d36cc
Mikhail Sushchikh <sumux200013@gmail.com>
2023-02-01 19:21:44 +0400

    refactor modal via React.Portals

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -2,24 +2,44 @@ import styles from './ErrorModal.module.css'
 import Card from "./Card";
 import Button from "./Button";
 import {Fragment} from "react";
+import {createPortal} from "react-dom";
+
+const Backdrop = (props) => {
+    return (<div className={styles.backdrop} onClick={props.onConfirm}/>);
+};
+
+const ModalOverlay = (props) => {
+    return (
+        <Card className={styles.modal}>
+            <header className={styles.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={styles.content}>
+                <p>{props.message}</p>
+            </div>
+            <footer className={styles.actions}>
+                <Button onClick={props.onConfirm}>OK</Button>
+            </footer>
+        </Card>
+    );
+};
+
 
 const ErrorModal = (props) => {
     return (
         <Fragment>
-            <div className={styles.backdrop} onClick={props.onConfirm} />
-            <Card className={styles.modal}>
-                <header className={styles.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={styles.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={styles.actions}>
-                    <Button onClick={props.onConfirm}>OK</Button>
-                </footer>
-            </Card>
+            {createPortal(
+                <Backdrop onConfirm={props.onConfirm}/>,
+                document.getElementById('backdrop-root')
+            )}
+            {createPortal(
+                <ModalOverlay
+                    title={props.title}
+                    message={props.message}
+                    onConfirm={props.onConfirm}/>,
+                document.getElementById('overlay-root')
+            )}
         </Fragment>
-
     );
 };
 
